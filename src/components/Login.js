f130d447ac0b51a9012c49a1370ba07eac755b86
khalid <khalidtwfik66@yahoo.com>
2024-03-22 23:12:30 +0200

    first commit

---
+++ src/components/Login.js
@@ -0,0 +1,60 @@
+import { faEye, faEyeSlash } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+function Login() {
+
+    const [email, setemail] = useState('');
+    const [Password, setpassword] = useState('');
+    const [accept, setaccept] = useState(false);
+    const [show, setshow] = useState(false);
+    console.log(email)
+
+    function submit(e) {
+        e.preventDefault();
+        setaccept(true)
+    }
+    function showpass() {
+        setshow(!show)
+    }
+
+    return (
+        <>
+            <h1>SIGN IN</h1>
+            <div className="card">
+                <form onSubmit={submit}>
+
+
+                    <div className="input-box">
+                        <h4>Email</h4>
+                        <input type="Email"
+                            placeholder="Email"
+                            required
+                            value={email}
+                            onChange={(e) => setemail(e.target.value)}
+                        />
+                    </div>
+                    <div className="input-box">
+                        <h4>Password</h4>
+                        <input type={show?"text" :"Password" }
+                            placeholder="Password"
+                            required value={Password}
+                            onChange={(e) => setpassword(e.target.value)}
+                            id="pass"
+                        />
+
+                        {Password.length < 8 && accept && <h5 className="error">Password must be more than 8 characters</h5>}
+                    </div>
+                    
+                    <p id="show" onClick={showpass}>{show ? <FontAwesomeIcon icon={faEyeSlash} />
+                        : <FontAwesomeIcon icon={faEye} />}</p>
+                    <button type="submit" className="btn">SIGN IN</button>
+                    <p>Don't you have account?<Link to={"sign"}>Create One</Link></p>
+
+                </form>
+            </div>
+        </>
+    )
+}
+export default Login;
---
+++ src/components/Sign-in.js
@@ -0,0 +1,79 @@
+import { faEye, faEyeSlash } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+function Sign() {
+    const [firstName, setfirstName] = useState('');
+    const [secondName, setsecondName] = useState('');
+    const [email, setemail] = useState('');
+    const [Password, setpassword] = useState('');
+    const [accept, setaccept] = useState(false);
+    const [show, setshow] = useState(false);
+
+
+    function submit(e) {
+        e.preventDefault()
+        setaccept(true)
+    }
+    function showpass() {
+        setshow(!show)
+    }
+
+    return (
+        <>
+            <h1>Create your account</h1>
+            <div className="card">
+
+                <form onSubmit={submit}  >
+
+                    <div className="input-box">
+                        <h4>First Name</h4>
+                        <input type="text"
+                            placeholder="First Name"
+                            value={firstName}
+                            onChange={(e) => setfirstName(e.target.value)}
+                        />
+                        {firstName === '' && accept && <h5 className="error">firstName is empty</h5>}
+                    </div>
+                    <div className="input-box">
+                        <h4>Second Name</h4>
+                        <input type="text"
+                            placeholder="Second Name"
+                            value={secondName}
+                            onChange={(e) => setsecondName(e.target.value)}
+
+                        />
+                        {secondName === '' && accept && <h5 className="error">secondName is empty</h5>}
+                    </div>
+                    <div className="input-box">
+                        <h4>Email</h4>
+                        <input type="Email"
+                            placeholder="Email"
+                            required
+                            value={email}
+                            onChange={(e) => setemail(e.target.value)}
+                        />
+                        {email === '' && accept && <h5 className="error">email is empty</h5>}
+                    </div>
+                    <div className="input-box">
+                        <h4>Password</h4>
+                        <input type={show ? "text" : "Password"}
+                            placeholder="Password"
+                            value={Password}
+                            onChange={(e) => setpassword(e.target.value)}
+                        />
+                        {Password.length < 8 && accept && <h5 className="error">Password must be more than 8 characters</h5>}
+                    </div>
+                    <p id="show" on onClick={showpass}>{show?<FontAwesomeIcon icon={faEyeSlash}/>:<FontAwesomeIcon icon={faEye} /> }</p>
+                    <button type="submit" className="btn">Create</button>
+                    <p>Do you have account?<Link to={"/"}>Sign In</Link></p>
+
+                </form>
+            </div>
+        </>
+    )
+
+}
+
+export default Sign;
