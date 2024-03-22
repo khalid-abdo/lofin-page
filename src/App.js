675eb4c475d0149bf21d5038645c0315ff1e7ef4
khalid <khalidtwfik66@yahoo.com>
2024-03-23 00:52:47 +0200

    new

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ import Login from './components/Login';
 function App() {
   return (
     <div className="App">
-      
+      <Router>
       <Routes>
         <Route index element={<Login/> } />
         <Route path='/' element={<Login />} />
         <Route path='sign' element={<Sign />} />
-      </Routes>
+        </Routes>
+        </Router>
 
     </div>
   );
